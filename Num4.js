8dcd50933248d27aec833255d943c22c1a7895e6
Giselle <gisalina0618@gmail.com>
2022-11-30 23:09:17 -0400

    Num4

---
+++ Num4.js
@@ -0,0 +1,15 @@
+//Crea un programa que pida dos números al usuario y responda si son iguales o no lo son.
+
+let num1=Number(prompt("Por Favor, ingrese un número."))
+let num2=Number(prompt("Por Favor, ingrese un número."))
+
+if(num1 == num2)
+{
+    msj1 = "LOS 2 NÚMEROS SON IGUALES";
+}
+else
+{
+    msj1 = "LOS 2 NÚMEROS SON DIFERENTES";
+}
+
+document.write("------------------------------------------------------------------------------------");
